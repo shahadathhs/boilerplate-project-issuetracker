748d530fc7bf781bf55b0d07eedfbf8c1cc0a9c6
shahadathhs <shahadathhossensajib732@gmail.com>
2024-08-16 12:15:36 +0600

    timeout error

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,31 @@
-const Issue = require('../models/Issue');
+const Issue = require("../models/Issue");
 
 module.exports = function (app) {
-  app.route('/api/issues/:project')
+  app
+    .route("/api/issues/:project")
     // GET request to view issues on a project with optional filters
     .get(async function (req, res) {
       const project = req.params.project;
-      const filter = { ...req.query, project };
+      const filter = { project, ...req.query };
 
-      try {
-        const issues = await Issue.find(filter).exec();
-        res.json(issues);
-      } catch (err) {
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
+      // Convert string 'true' and 'false' to boolean values
+      Object.keys(filter).forEach((key) => {
+        if (filter[key] === "true") filter[key] = true;
+        if (filter[key] === "false") filter[key] = false;
+      });
+
+      const issues = await Issue.find(filter).exec();
+      res.json(issues);
     })
 
     // POST request to create an issue with every field
-    .post(async function (req, res) { 
+    .post(async function (req, res) {
       const project = req.params.project;
-      const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+      const { issue_title, issue_text, created_by, assigned_to, status_text } =
+        req.body;
 
       if (!issue_title || !issue_text || !created_by) {
-        return res.json({ error: 'required field(s) missing' });
+        return res.json({ error: "required field(s) missing" });
       }
 
       const newIssue = new Issue({
@@ -33,46 +37,45 @@ module.exports = function (app) {
         project,
       });
 
-      try {
-        await newIssue.save();
-        res.json(newIssue);
-      } catch (err) {
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
+      await newIssue.save();
+      res.json(newIssue);
     })
 
     // PUT request to update one or multiple fields on an issue
     .put(async function (req, res) {
       const { _id, ...updateFields } = req.body;
 
-      if (!_id) return res.json({ error: 'missing _id' });
-      if (Object.keys(updateFields).length === 0) return res.json({ error: 'no update field(s) sent' });
+      if (!_id) return res.json({ error: "missing _id" });
+      if (Object.keys(updateFields).length === 0)
+        return res.json({ error: "no update field(s) sent", _id: _id });
 
-      const issue = await Issue.find({_id});
+      const issue = await Issue.findOne({ _id });
       if (!issue) {
-        return res.json({ error: 'could not delete', _id });
+        return res.json({ error: "could not update", _id: _id });
       }
-      const result = await Issue.updateOne({ _id }, { $set: { ...issue, updateFields }});
+
+      const result = await Issue.updateOne({ _id }, { $set: updateFields });
       if (result.modifiedCount === 0) {
-        return res.json({ error: 'could not update', _id });
+        return res.json({ error: "could not update", _id: _id });
       }
-      res.json({ result: 'successfully updated', _id });
+
+      res.json({ result: "successfully updated", _id: _id });
     })
 
     // DELETE request to delete an issue
     .delete(async function (req, res) {
       const { _id } = req.body;
       if (!_id) {
-        return res.json({ error: 'missing _id' });
+        return res.json({ error: "missing _id" });
       }
-      const issue = await Issue.find({_id});
+      const issue = await Issue.find({ _id });
       if (!issue) {
-        return res.json({ error: 'could not delete', _id });
+        return res.json({ error: "could not delete", _id });
       }
       const result = await Issue.deleteOne({ _id });
       if (result.deletedCount === 0) {
-        return res.json({ error: 'could not delete', _id });
+        return res.json({ error: "could not delete", _id });
       }
-      res.json({ result: 'successfully deleted', _id });
+      res.json({ result: "successfully deleted", _id });
     });
 };
